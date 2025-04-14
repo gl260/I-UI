e7a98888600e69ca88f310801336a2c26924f483
lgl <guanglong.li@cdatatec.com>
2025-04-14 15:21:59 +0800

    添加 JSXButton 组件，更新入口文件以支持 JSX 渲染，并在 Vite 配置中添加 JSX 插件

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue"
 
 // import IButton from './button'
-import IButton from './SFCButton.vue'
+// import IButton from './SFCButton.vue'
+import IButton from './JSXButton.jsx'
 
 createApp(IButton).mount('#app')
